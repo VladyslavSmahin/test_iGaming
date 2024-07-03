6d08969e15e5ac415800a23b12267fabb516c968
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-07-03 18:32:29 +0300

    test

---
+++ test_data_table/src/App.jsx
@@ -0,0 +1,14 @@
+import Nav from "./ui/components/Nav/Nav.jsx";
+import DataTable from "./ui/components/DataTable.jsx";
+import './app.scss'
+function App() {
+
+  return (
+    <div className='app'>
+      <Nav/>
+      <DataTable/>
+    </div>
+  )
+}
+
+export default App
